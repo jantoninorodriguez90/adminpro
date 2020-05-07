7dffe9f3733fb03cf15ca271b868091e74ecea65
Antonino Rodriguez <jantoninorodriguez@AntoninoRodriguez.local>
2020-05-07 10:50:38 -0700

    Modoficaciones en rutas y modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas01Component } from './pages/graficas01/graficas01.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 import { RegisterComponent } from './register/register.component';
-import { PagesComponent } from './pages/pages.component';
 
 
 const routes: Routes = [
-  { path: '', component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'graphic01', component: Graficas01Component },
-      { path: '', redirectTo: 'dashboard', pathMatch: 'full', }
-    ]
-  },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '**', component: NopagefoundComponent }
+  // { path: '**', component: NopagefoundComponent }
 ];
  
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { PagesModule } from './pages/pages.module';
+
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas01Component } from './pages/graficas01/graficas01.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { RegisterComponent } from './register/register.component';
-import { PagesComponent } from './pages/pages.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas01Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    RegisterComponent,
-    PagesComponent
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
